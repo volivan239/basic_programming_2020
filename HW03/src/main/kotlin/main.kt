0333df2ecb017aaefdbd9f6859279fa1efc408a1
Ivan Volkov <volkov.ivan2004@gmail.com>
2020-09-28 23:04:08 +0300

    Add lengths, sumsq, mapAccumL

--- HW03/src/main/kotlin/main.kt
+++ HW03/src/main/kotlin/main.kt
@@ -11,15 +11,30 @@ fun qsort(list: List <Int>): List <Int> {
 }
 
 fun reverse(list: List<Int>): List <Int> {
-    return list.foldRight(emptyList<Int>()) {elem, list -> list + elem}
+    return list.foldRight(emptyList()) {elem, pref -> pref + elem}
 }
 
 fun <T> List<T>.filterWithFold(f: (T)->Boolean): List<T> {
-    return this.fold(emptyList<T>()) {list, elem -> if (f(elem)) list + elem else list}
+    return this.fold(emptyList()) {list, elem -> if (f(elem)) list + elem else list}
 }
 
 fun <T> List<T>.filterWithFoldRight(f: (T)->Boolean): List<T> {
-    return this.foldRight(emptyList<T>()) {elem, lst -> if (f(elem)) listOf(elem) + lst else lst}
+    return this.foldRight(emptyList()) {elem, lst -> if (f(elem)) listOf(elem) + lst else lst}
+}
+
+fun lengths(strs: List<String>): List<Int> {
+    return strs.map { it.length }
+}
+
+fun sumsq(n: Int): Int {
+    var ans = 0
+    (1..n).map { ans += it * it }
+    return ans
+}
+
+fun <A,B,C> List<B>.mapAccumL(f: (A, B)->Pair<A,C>, init: A): Pair<A, List<C>> {
+    val res = Pair(init, emptyList<C>())
+    return fold(res) {cur, it -> Pair(f(cur.first, it).first, cur.second + f(cur.first, it).second)}
 }
 
 fun main() {
@@ -30,7 +45,7 @@ fun main() {
         return
     }
     try {
-        arr = inp.split(" ").fold(emptyList<Int>()) {list, str -> list + str.toInt()}
+        arr = inp.split(" ").fold(emptyList()) {list, str -> list + str.toInt()}
     } catch (e: Exception) {
         println("Incorrect input")
         return
--- HW03/src/test/kotlin/mainTest.kt
+++ HW03/src/test/kotlin/mainTest.kt
@@ -37,22 +37,52 @@ internal class mainTest {
     }
     @Test
     fun filterFold() {
-        var a = listOf("a", "aba", "abacaba", "ax", "abc")
+        val a = listOf("a", "aba", "abacaba", "ax", "abc")
         assertEquals(a.filter { it.length > 2 }, a.filterWithFold { it.length > 2 })
     }
     @Test
     fun filterFoldEmpty() {
-        var a = listOf(0, 1, 2, 3)
+        val a = listOf(0, 1, 2, 3)
         assertEquals(a.filterWithFold{it < 0}, emptyList<Int>())
     }
     @Test
     fun filterFoldRight() {
-        var a = listOf("a", "aba", "abacaba", "ax", "abc")
+        val a = listOf("a", "aba", "abacaba", "ax", "abc")
         assertEquals(a.filter { it.length > 2 }, a.filterWithFoldRight { it.length > 2 })
     }
     @Test
     fun filterFoldRightEmpty() {
-        var a = listOf(0, 1, 2, 3)
+        val a = listOf(0, 1, 2, 3)
         assertEquals(emptyList<Int>(), a.filterWithFoldRight{it < 0})
+    }
+    @Test
+    fun lengths() {
+        val arr = listOf("abacaba", "", "ab")
+        val expected = listOf(7, 0, 2)
+        assertEquals(expected, lengths(arr))
+    }
+    @Test
+    fun sumsq1() {
+        assertEquals(1, sumsq(1))
+    }
+    @Test
+    fun sumsq() {
+        val t = 1007
+        var summ = 0
+        for (i in 1..t)
+            summ += i * i
+        assertEquals(summ, sumsq(t))
+    }
+    @Test
+    fun mapAccumLEx1() {
+        val arr = listOf(9, 6, 3)
+        val expected = Pair(5, listOf(45, 30, 15))
+        assertEquals(expected, arr.mapAccumL({a, b -> Pair(a, a * b)}, 5))
+    }
+    @Test
+    fun mapAccumLEx2() {
+        val arr = listOf(2, 4, 8)
+        val expected = Pair(19, listOf(10, 28, 88))
+        assertEquals(expected, arr.mapAccumL({a, b -> Pair(a + b, a * b)}, 5))
     }
 }
